efdfb6b23c805bb17420f3ecb1a1be5071800fcd
unknown <08041@sinoltd.local>
2026-04-08 16:54:47 +0800

    Auto create default superuser on startup

--- scoring_app/apps.py
+++ scoring_app/apps.py
@@ -1,5 +1,16 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+def create_default_admin(sender, **kwargs):
+    from django.contrib.auth.models import User
+    try:
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+    except Exception:
+        pass
 
 class ScoringAppConfig(AppConfig):
     name = 'scoring_app'
+
+    def ready(self):
+        post_migrate.connect(create_default_admin, sender=self)
